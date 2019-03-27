3a1a257ef1a957ed822c230ca5824cfe10d14ec1
eliavw <elia.vw@gmail.com>
2019-03-27 16:13:45 +0100

    better example

--- scripts/example.py
+++ scripts/example.py
@@ -4,15 +4,13 @@ This is a dummy script of a 'solution' of the first 3 queries.
 
 
 def query_01(connection, column_names):
-
     return None
 
 
 def query_02(connection, column_names, datum='1980-01-16'):
-
-
     return None
 
 
 def query_03(connection, column_names):
     return None
+
